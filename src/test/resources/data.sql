0dd2947e9cec463aecf464b4eb9cc5a3577e7277
Tomas1205 <tomas.penxa@gmail.com>
2023-10-06 21:42:37 +0200

    setup and first test

---
+++ src/test/resources/data.sql
@@ -0,0 +1,6 @@
+insert into customers(customer_id,FirstName,LastName,Email,Phone) values 
+('6fd4b523-c145-55ea-94d9-a67045029162', 'Lukas','dd','xx@gmail.com','+421459034456256'),
+('5fd4c523-c178-55ea-94d9-a67045029162', 'Tomas','XX','dsa@gmail.com','+56455554544'),
+('4fd4c523-c178-55ea-94d9-a67045029162', 'Peter','Hale','ds@Yahoo.com', '+585456425'),
+('3fd4c523-c178-55ea-94d9-a67045029162', 'Jozef','Fuska','fuska@joyev.com', '+421456456456'),
+('2fd4c523-c178-55ea-94d9-a67045029162', 'Marek','Hulka','hulka@azet.sk', '+421456456456');
