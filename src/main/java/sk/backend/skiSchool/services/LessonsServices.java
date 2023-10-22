c06dccd45fab32b34afd352e760ffc3011c862e0
Tomas1205 <tomas.penxa@gmail.com>
2023-10-22 20:35:14 +0200

    fixed the service

--- src/main/java/sk/backend/skiSchool/services/LessonsServices.java
+++ src/main/java/sk/backend/skiSchool/services/LessonsServices.java
@@ -2,7 +2,7 @@ package sk.backend.skiSchool.services;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
-
+import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Service;
 
 import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
+import sk.backend.skiSchool.model.Instructors;
 import sk.backend.skiSchool.model.Lessons;
+import sk.backend.skiSchool.repository.InstructorsRepository;
 import sk.backend.skiSchool.repository.LessonsRepository;
 
 @Service
@@ -20,6 +22,9 @@ public class LessonsServices {
 
     @Autowired
     private LessonsRepository lessonsRepository;
+
+    @Autowired
+    private InstructorsRepository instructorsRepository;
 
     public boolean createLesson(UUID instructor_id, String lessonType, int lessonLevel, int capacity, int price, ZonedDateTime startTime, ZonedDateTime endTime){
         if (instructor_id == null) {
@@ -50,7 +55,10 @@ public class LessonsServices {
         
         log.info("Instructor id: " + instructor_id);
 
-        Set<Lessons> instructorLessons = lessonsRepository.findAllByInstructorId(instructor_id);        
+        Set<Lessons> instructorLessons = lessonsRepository.findAllByInstructorId(instructor_id);
+        Optional<Instructors> instructor = instructorsRepository.findById(instructor_id);
+        
+        
 
         
         for (Lessons lesson : instructorLessons) {
@@ -70,13 +78,14 @@ public class LessonsServices {
 
         try {
             log.info("Creating lesson");
-            Lessons = Lessons.builder()
+            Lessons newLesson = Lessons.builder()
                 .lessonType(lessonType)
                 .lessonLevel(lessonLevel)
                 .maxCapacity(capacity)
                 .price(price)
                 .startTime(startTime)
                 .endTime(endTime)
+                .instructors(Set.of(instructor.get()))
                 .build();
 
             lessonsRepository.save(newLesson);
